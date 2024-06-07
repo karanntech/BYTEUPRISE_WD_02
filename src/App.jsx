462f4a03f2022c21b49c5e517f84f5ca1b395d3b
karann09 <karankumar011kp@gmail.com>
2024-06-07 23:10:42 +0530

    testimonial created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 import Workflow from "./components/Workflow";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
     <FeatureSection/>
     <Workflow/>
     <Pricing/>
+    <Testimonials/>
     </div>
     </>
   )
